f2e0584074b39dd6a9d7ba731dc2a5d4c53412c5
Benjamin Congdon <bencongdon96@gmail.com>
2020-12-14 21:39:09 -0800

    Minor refactor to day15_rs

--- Day10-19/day15_rs/src/main.rs
+++ Day10-19/day15_rs/src/main.rs
@@ -1,3 +1,5 @@
+#![feature(entry_insert)]
+use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 
 fn nth_spoken_number(seed: &[u64], nth: usize) -> u64 {
@@ -8,12 +10,13 @@ fn nth_spoken_number(seed: &[u64], nth: usize) -> u64 {
         .collect::<HashMap<u64, usize>>();
     let mut most_recent = seed[seed.len() - 1];
     for idx in (seed.len() - 1)..(nth - 1) {
-        let spoken = if let Some(most_recent_time) = last_spoken.get(&most_recent) {
-            idx - *most_recent_time
+        let entry = last_spoken.entry(most_recent);
+        let spoken = if let Entry::Occupied(ref most_recent_time) = entry {
+            idx - most_recent_time.get()
         } else {
             0
         };
-        last_spoken.insert(most_recent, idx);
+        entry.insert(idx);
         most_recent = spoken as u64;
     }
     most_recent
